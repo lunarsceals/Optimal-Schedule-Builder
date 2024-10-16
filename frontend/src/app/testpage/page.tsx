f9e27586d7e93f9b6690fc6e9b976a00b7513992
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-10-16 19:03:01 -0400

    Create page to test AddCourses component

---
+++ frontend/src/app/testpage/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddCourses from "../component/AddCourses";
+
+//TODO: Delete this page later; this is a test page to test component
+const AddComponentTestPage = () => {
+  return (
+    <div>
+      <AddCourses />
+    </div>
+  );
+};
+
+export default AddComponentTestPage;
