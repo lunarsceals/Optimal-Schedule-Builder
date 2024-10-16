f18b6c90267d05e20aad14ea971c382689fe97ce
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-10-16 19:02:29 -0400

    Create TabButtonComponent

---
+++ frontend/src/app/component/TabButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Prop {
+  tabName: string;
+}
+
+const TabButton = (prop: Prop) => {
+  return (
+    <button className="flex-1 text-nowrap rounded-t-md bg-red-400 px-6 py-1 text-justify">
+      By {prop.tabName}
+    </button>
+  );
+};
+
+export default TabButton;
